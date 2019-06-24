a23ee06075558fb3015b7c0eab8540c4578e77d1
claxtont <claxtons4@gmail.com>
2019-06-24 16:11:48 -0500

    Initial commit

---
+++ TTSBot/CommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace TTSBot
+{
+    class CommandHandler
+    {
+        DiscordSocketClient _client;
+        CommandService _service;
+
+        public async Task InitializeAsync(DiscordSocketClient client)
+        {
+            _client = client;
+            _service = new CommandService();
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly());
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private Task HandleCommandAsync(SocketMessage s)
+        {
+
+        }
+    }
+}
